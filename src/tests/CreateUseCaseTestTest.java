94e862b102ac615c1c72797b295e226ad5d851f8
Kostas-Papakostas <papakostask@hotmail.com>
2020-05-02 22:41:42 +0300

    upLoaded New Tests

--- src/tests/CreateUseCaseTestTest.java
+++ src/tests/CreateUseCaseTestTest.java
@@ -1,48 +1,66 @@
 package tests;
 
-import static org.junit.Assert.*;
+import gui.tableElements.commons.MyTableModel;
+import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.junit.Test;
+//import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import static org.junit.Assert.assertEquals;
 
 public class CreateUseCaseTestTest {
+
+    FakeGui fakeGui = new FakeGui();
+
+
+    @Test
+    public void testCreatedFile(){
+        String expectedContenrts = "Project-name:lala\n"+
+                "Dataset-txt:filesHandler/datasets/Atlas.txt\n"+
+                "Input-csv:filesHandler/input/atlas.csv\n"+
+                "Assessement1-output:filesHandler/output/atlas_Assessment1.txt\n"+
+                "Assessement2-output:filesHandler/output/atlas_Assessment1.txt\n"+
+                "Transition-xml:filesHandler/transitions/atlasTransitions.xml";
+
+        File file = fakeGui.getFileToReturn();
+        String file1str=null;
+
+        try {
+            file1str = new String(Files.readAllBytes(Paths.get("./filesHandler/inis/lala.ini")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(expectedContenrts,file1str);
+    }
+
 
     @Test
     public void testDumpData_generalModel(){
 
+        //FakeGui fakeGui = new FakeGui();
 
         String file1str = null;
-        try {
-            file1str = new String(Files.readAllBytes(Paths.get("./Dump Files Original/generalModel_Atlas.txt")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         String file2str = null;
+        String pathToOriginalFile = "./Dump Files Original/Load Use Case/generalModel_Atlas.txt";
+        String pathToRefactoredFile = "./Dump Files Refactored/Create Use Case/generalModel_Atlas.txt";
+
+        MyTableModel fakeGeneralModel = fakeGui.getGeneralModel();
+        fakeGeneralModel.dumpData(pathToRefactoredFile);
+
         try {
-            file2str = new String(Files.readAllBytes(Paths.get("./Dump Files Refactored/generalModel_Atlas.txt")));
+            file1str = new String(Files.readAllBytes(Paths.get(pathToOriginalFile)));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        assertEquals(file1str, file2str);
-    }
-
-    @Test
-    public void testDumpData_zoomModel(){
-
-
-        String file1str = null;
         try {
-            file1str = new String(Files.readAllBytes(Paths.get("./Dump Files Original/zoomModel_Atlas.txt")));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String file2str = null;
-        try {
-            file2str = new String(Files.readAllBytes(Paths.get("./Dump Files Refactored/zoomModel_Atlas.txt")));
+            file2str = new String(Files.readAllBytes(Paths.get(pathToRefactoredFile)));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/tests/FakeGui.java
+++ src/tests/FakeGui.java
@@ -1,14 +1,18 @@
 package tests;
 
-import java.io.File;
 import gui.actionListeners.TableController;
 import gui.mainEngine.Gui;
 import gui.tableElements.commons.MyTableModel;
 
+import java.io.File;
+
 
 public class FakeGui extends Gui{
     private static final long serialVersionUID = 1L;
-    
+
+
+
+    private File fileToReturn;
     private MyTableModel detailedModel;
     
     public FakeGui(){
@@ -16,9 +20,31 @@ public class FakeGui extends Gui{
         getInfoFromFileController();
         fillTable();
         fillTree();
-        constructDetailedModel();  
+        constructDetailedModel();
+        createProject();
     }
-    
+
+    public File createProject(){
+
+        FakeCreateProjectJDialog createProjectDialog = new FakeCreateProjectJDialog("lala", "filesHandler/datasets/Atlas.txt", "filesHandler/input/atlas.csv", "filesHandler/output/atlas_Assessment1.txt", "filesHandler/output/atlas_Assessment1.txt", "filesHandler/transitions/atlasTransitions.xml");
+
+        createProjectDialog.setModal(true);
+
+        createProjectDialog.setVisible(true);
+        File file;
+        if (true) {
+
+            //createProjectDialog.setVisible(true);
+
+            file = createProjectDialog.getFile();
+            System.out.println(file.toString());
+            //TODO some kind of test? It the same with load project
+            project = fileController.createAndEditProjectAction(FakeGui.this, file);
+
+        }
+        fileToReturn=file;
+        return file;
+    }
     
     private void loadProject() {
         //String fileName;
@@ -26,7 +52,10 @@ public class FakeGui extends Gui{
         //fileName = file.toString();
         fileController.loadProjectAction(true, file);
     }
-    
+
+    public File getFileToReturn() {
+        return fileToReturn;
+    }
     
     private void constructDetailedModel() {
         TableController tableController = TableController.getInstance();
