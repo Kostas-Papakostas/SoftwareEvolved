875799cf5669af8833499762638719278ec37567
Giorgios Vardakas <geoo1995@gmail.com>
2020-06-14 00:31:48 +0300

    Delete TablesInterface.java

--- data/configurators/configurators/TablesInterface.java
+++
@@ -1,7 +0,0 @@
-package data.configurators;
-
-import gui.tableElements.commons.MyTableModel;
-
-public interface TablesInterface {
-    public MyTableModel createTableModel(String[][] finalRowsZoomArea_p, String[] finalColumnsZoomArea_p);
-}
